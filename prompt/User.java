afd8762d348035ad29d4907d85ea9d69bffcdf23
BuenoVini <you@example.com>
2020-08-12 14:04:21 -0300

    class User now has its own file

---
+++ prompt/User.java
@@ -0,0 +1,27 @@
+public class User
+{
+    /* --class attributes-- */
+    private String name;
+    private String surname;
+    private String email;
+    private String phone_number;
+    private String login_user;
+    /* --class attributes-- */
+
+    /* --constructors-- */
+    public User(String name, String surname, String email, String phone_number, String login_user)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.login_user = login_user;
+    }
+    /* --constructors-- */
+
+    /* --setters-- */
+    /* --setters-- */
+    
+    /* --getters-- */
+    /* --getters-- */
+}
