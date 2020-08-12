425b3d9a0185d82062ec51a437d31e0d1e1558d5
BuenoVini <you@example.com>
2020-08-12 15:22:35 -0300

    created new Reminder class

---
+++ prompt/Reminder.java
@@ -0,0 +1,31 @@
+public class Reminder
+{
+    /* --class attributes-- */
+    protected String title;
+    protected String description;
+    protected boolean finished;
+    /* --class attributes-- */
+
+    /* --constructors-- */
+    protected Reminder(String title, String description)
+    {
+        this.title = title;
+        this.description = description;
+        this.finished = false;
+    }
+    /* --constructors-- */
+
+    /* --public methods-- */
+    /* --public methods-- */
+
+    /* --private methods-- */
+    /* --private methods-- */
+
+    /* --setters-- */
+    protected void set_finished() { this.finished = true; }
+    protected void set_unfinished() { this.finished = false; }
+    /* --setters-- */
+    
+    /* --getters-- */
+    /* --getters-- */
+}
