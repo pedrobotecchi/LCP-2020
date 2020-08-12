60fb5d3774c3661fdc128c59d5e4ff910866dc98
BuenoVini <you@example.com>
2020-08-12 15:21:36 -0300

    changed find_user() and added add_reminder()

--- prompt/Engine.java
+++ prompt/Engine.java
@@ -6,6 +6,8 @@ public class Engine
     /* --class attributes-- */
     private static HashMap<Integer, User> hash_users;
     private static TreeSet<String> tree_login_user;
+
+    private User current_user;
     /* --class attributes-- */
 
     /* --constructors-- */
@@ -17,7 +19,7 @@ public class Engine
     /* --constructors-- */
 
     /* --public methods-- */
-    // add user to the hashmap: true if successfully added -> false if not
+    // add user to the hashmap: returns true if successfully added | false if not
     public boolean add_user(String name, String surname, String email, String phone_number, String login_user, String login_password)
     {
         if ( !tree_login_user.contains(login_user) )
@@ -32,18 +34,30 @@ public class Engine
         else return false;
     }
 
-    // find if a user exists: true if it does -> false if it doesn't
-    public boolean find_user(String login_user, String login_password)
+    // try to login with a user: returns true if the user is found and set the current_user | false if it doesn't find
+    public boolean login_user(String login_user, String login_password)
     {
         int id = (login_user + login_password).hashCode();
 
         if ( hash_users.containsKey(id) )
+        {
+            this.current_user = hash_users.get(id);
             return true;
+        }
         else return false;
+    }
+
+    // add a new reminder to the selected user
+    public void add_reminder(String title, String description)
+    {
+        if ( this.current_user != null )
+            this.current_user.new_reminder(title, description);
+        // else System.out.printf("\n>> Login first\n"); // TODO: throw exception if current_user == null (login_user was never called or returned false)
     }
     /* --public methods-- */
     
     /* --private methods-- */
+    // search in hash_users the id passed
     /* --private methods-- */
 
     /* --setters-- */
