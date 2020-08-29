216c96872abda3d7183e6387b8c25c6c3f76fbf9
BuenoVini <you@example.com>
2020-08-28 22:07:16 -0300

    implemented close_reminder()

--- prompt/User.java
+++ prompt/User.java
@@ -9,7 +9,8 @@ public class User
     private String phone_number;
     private String login_user;
 
-    private ArrayList<Reminder> reminders;
+    private ArrayList<Reminder> due_reminders;
+    private ArrayList<Reminder> closed_reminders;
     /* --class attributes-- */
 
     /* --constructors-- */
@@ -21,16 +22,26 @@ public class User
         this.phone_number = phone_number;
         this.login_user = login_user;
 
-        this.reminders = new ArrayList<Reminder>();
+        this.due_reminders = new ArrayList<Reminder>();
+        this.closed_reminders = new ArrayList<Reminder>();
     }
     /* --constructors-- */
 
     /* --public methods-- */
-    public void new_reminder(String title, String description)
+    public void add_reminder(String title, String description)
     {
-        this.reminders.add(new Reminder(title, description));
+        this.due_reminders.add(new Reminder(title, description));
     }
 
+    public void close_reminder(int index)
+    {
+        Reminder reminder = this.due_reminders.get(index);
+
+        reminder.set_finished();
+        this.closed_reminders.add(reminder);
+
+        this.due_reminders.remove(index);
+    }
     /* --public methods-- */
     
     /* --private methods-- */
@@ -40,6 +51,8 @@ public class User
     /* --setters-- */
     
     /* --getters-- */
-    public ArrayList<Reminder> get_reminders() { return this.reminders; }
+    public ArrayList<Reminder> get_due_reminders() { return this.due_reminders; }
+    public ArrayList<Reminder> get_closed_reminders() { return this.closed_reminders; }
+    // TODO: add getters for the attributes
     /* --getters-- */
 }
